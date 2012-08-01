ef6ad0c0fefe89101af7d51cbb4ae2f3314f016a
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-01 21:59:57 +0100

    Next commit shiz... Attemp add of delay

--- HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Spells/WingardiumLeviosa.java
+++ HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Spells/WingardiumLeviosa.java
@@ -1,5 +1,6 @@
 package com.lavacraftserver.HarryPotterSpells.Spells;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class WingardiumLeviosa {
@@ -8,7 +9,15 @@ public class WingardiumLeviosa {
 		if(p.isFlying()) {
 			p.setFlying(false);
 		} else {
+			Location l = p.getLocation();
+			p.teleport(new Location(p.getWorld(), l.getX(), l.getY() + 1, l.getZ()));
 			p.setFlying(true);
+			myPlugin.getServer().getScheduler().scheduleSyncDelayedTask(myPlugin, new Runnable() {
+
+				   public void run() {
+				       getServer().broadcastMessage("This message is broadcast by the main thread");
+				   }
+				}, 300L);
 		}
 	}
 
