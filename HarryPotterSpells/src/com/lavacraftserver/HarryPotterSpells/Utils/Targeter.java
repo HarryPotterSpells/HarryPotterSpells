07e36eea02f66a0be5c8a8140b295886a04669ec
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-01 14:55:55 +0100

    Created Targeter util (go me)

---
+++ HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Utils/Targeter.java
@@ -0,0 +1,29 @@
+package com.lavacraftserver.HarryPotterSpells.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.lavacraftserver.HarryPotterSpells.Spell;
+import com.lavacraftserver.HarryPotterSpells.Spells.Confundus;
+
+public class Targeter extends JavaPlugin implements Listener {
+	public static Map<UUID, Spell> NeedsTargeted = new HashMap<UUID, Spell>();
+	
+	@EventHandler
+	public void onEntityDamageEntityEvent(EntityDamageByEntityEvent e) {
+		Entity victim = e.getEntity();
+		Entity attacker = e.getDamager();
+		if(NeedsTargeted.containsKey(attacker.getUniqueId())) {
+			Spell spell = NeedsTargeted.get(attacker.getUniqueId());
+			if(spell == Spell.CONFUNDUS) {Confundus.hit(victim);}
+		}
+	}
+
+}
