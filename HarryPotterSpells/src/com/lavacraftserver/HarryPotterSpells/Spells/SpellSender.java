4385edbbf58b24e8a7d6ccf05cfc81119d415ac8
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-01 21:48:37 +0100

    Added confringo and WingardiumLeviosa

--- HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Spells/SpellSender.java
+++ HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Spells/SpellSender.java
@@ -9,5 +9,7 @@ public class SpellSender {
 		if(spell.equalsIgnoreCase("confringo")) {Confringo.cast(p, e);}
 		if(spell.equalsIgnoreCase("time")) {TimeSpell.cast(p, e);}
 		if(spell.equalsIgnoreCase("tree")) {TreeSpell.cast(p);}
+		if(spell.equalsIgnoreCase("confundus")) {Confundus.cast(p);}
+		if(spell.equalsIgnoreCase("wingardiumleviosa")) {WingardiumLeviosa.cast(p);}
 	}
 }
