bb0cbc2c27ec1c64bc9d8a053fccf5e42a5a79e9
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-01 21:38:16 +0100

    Added funky lightning when you cast this spell

--- HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Spells/TimeSpell.java
+++ HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Spells/TimeSpell.java
@@ -1,6 +1,8 @@
 package com.lavacraftserver.HarryPotterSpells.Spells;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -12,22 +14,33 @@ public class TimeSpell {
 	//Night == 15000L
 
 	public static void cast(Player p, PlayerInteractEvent e) {
-		if(e.getClickedBlock() != null) {
-			if(e.getClickedBlock().getType() == Material.GLOWSTONE) {
-				p.getWorld().setTime(0L);
+		World w = p.getWorld();
+		if(p.getEyeLocation().getBlock() != null) {
+			if(p.getEyeLocation().getBlock().getType() == Material.GLOWSTONE) {
+				w.setTime(0L);
 				return;
-			} else if(e.getClickedBlock().getType() == Material.OBSIDIAN) {
-				p.getWorld().setTime(15000L);
+			} else if(p.getEyeLocation().getBlock().getType() == Material.OBSIDIAN) {
+				w.setTime(15000L);
 				return;
 			}
 		}
-		long time = p.getWorld().getTime();
+		long time = w.getTime();
 		if(time < 12000) {
 			time = time + 12000L;
 		} else {
 			time = time - 12000L;
 		}
-		p.getWorld().setTime(time);
+		awesomeLightning(p.getLocation(), w);
+		w.setTime(time);
+	}
+
+	public static void awesomeLightning(Location l, World w) {
+		double x = l.getX(), y = l.getY(), z = l.getZ();
+		//X Y Z
+		w.strikeLightningEffect(new Location(w, x, y, z - 2));
+		w.strikeLightningEffect(new Location(w, x, y, z + 2));
+		w.strikeLightningEffect(new Location(w, x - 2, y, z));
+		w.strikeLightningEffect(new Location(w, x + 2, y, z));
 	}
 	
 }
