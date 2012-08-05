182478d26d8a5d33e760ccf34030ba5e097c1068
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-05 18:28:39 +0100

    Added LogBlock Hook. Fixed #24

---
+++ HarryPotterSpells/src/com/lavacraftserver/HarryPotterSpells/Hooks/LogBlock.java
@@ -0,0 +1,29 @@
+package com.lavacraftserver.HarryPotterSpells.Hooks;
+
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import com.lavacraftserver.HarryPotterSpells.PM;
+
+import de.diddiz.LogBlock.Consumer;
+
+public class LogBlock {
+	public static Consumer LogBlockConsumer;
+	public static de.diddiz.LogBlock.LogBlock LogBlockPlugin;
+	
+	public static void setupLogBlock() {
+		if(PM.hps.getConfig().getBoolean("LogBlockEnabled")) {
+			Plugin plugin = Bukkit.getServer().getPluginManager().getPlugin("LogBlock");
+			if(plugin != null) {
+				LogBlockConsumer = ((de.diddiz.LogBlock.LogBlock)plugin).getConsumer();
+				LogBlockPlugin = (de.diddiz.LogBlock.LogBlock)plugin;
+			} else {
+				PM.log("Could not hook into LogBlock. LogBlock features have been disabled.", Level.WARNING);
+				PM.hps.getConfig().set("LogBlockEnabled", false);
+			}
+		}
+	}
+
+}
