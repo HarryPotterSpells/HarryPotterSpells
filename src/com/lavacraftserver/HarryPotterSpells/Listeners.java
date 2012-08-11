a8f106a22e0b791cb86e967d2682e7abf28e40c5
Nate Mortensen <nate.richard.mortensen@gmail.com>
2012-08-11 14:21:22 -0600

    Removed unnecessary import.

--- src/com/lavacraftserver/HarryPotterSpells/Listeners.java
+++ src/com/lavacraftserver/HarryPotterSpells/Listeners.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
