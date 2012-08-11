2aca282b99d67a4cb0e6481844500ecf14cfa10a
Nate Mortensen <nate.richard.mortensen@gmail.com>
2012-08-11 12:38:53 -0600

    Fixed RetentionPolicy of Load.

--- src/com/lavacraftserver/HarryPotterSpells/api/Load.java
+++ src/com/lavacraftserver/HarryPotterSpells/api/Load.java
@@ -3,6 +3,10 @@
  */
 package com.lavacraftserver.HarryPotterSpells.api;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Load {
 	public boolean value();
 	
