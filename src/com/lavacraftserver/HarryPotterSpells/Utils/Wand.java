f9d972f930c5fe6b96d85bb13f9b8744772a7eed
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-30 11:37:42 +0100

    Added check for custom wand. Fixed #65

---
+++ src/com/lavacraftserver/HarryPotterSpells/Utils/Wand.java
@@ -0,0 +1,37 @@
+package com.lavacraftserver.HarryPotterSpells.Utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+
+import com.lavacraftserver.HarryPotterSpells.HarryPotterSpells;
+
+public class Wand {
+	HarryPotterSpells plugin;
+	
+	public Wand(HarryPotterSpells instance) {
+		plugin = instance;
+	}
+	
+	public boolean isWand(ItemStack i) {
+		if(i.getTypeId() == plugin.getConfig().getInt("wand-id", 280)) {
+			if(plugin.getConfig().getBoolean("apply-wand-enchantment", false)) {
+				Map<Enchantment, Integer> wand = new HashMap<Enchantment, Integer>();
+				wand.put(Enchantment.DURABILITY, 1);
+				Map<Enchantment, Integer> item = (HashMap<Enchantment, Integer>)i.getEnchantments();
+				if(wand == item) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
+
+}
