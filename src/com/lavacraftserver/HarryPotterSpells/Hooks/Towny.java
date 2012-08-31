d3f55ab7963cb7185840622dec0a7bb6842aec87
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-31 11:22:44 +0100

    Added Towny hook #67

---
+++ src/com/lavacraftserver/HarryPotterSpells/Hooks/Towny.java
@@ -0,0 +1,36 @@
+package com.lavacraftserver.HarryPotterSpells.Hooks;
+
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import com.lavacraftserver.HarryPotterSpells.HarryPotterSpells;
+
+public class Towny {
+	HarryPotterSpells plugin;
+
+	public Towny(HarryPotterSpells instance) {
+		plugin = instance;
+	}
+	
+	com.palmergames.bukkit.towny.Towny Towny;
+	
+	public void setupTowny() {
+		if(plugin.getConfig().getBoolean("TownyEnabled") != false) {
+			Plugin townyplugin = Bukkit.getServer().getPluginManager().getPlugin("Towny");
+	 
+			// Towny may not be loaded
+			if (townyplugin == null || !(townyplugin instanceof Towny)) {
+				plugin.PM.log("Could not hook into Towny. Towny features have been disabled.", Level.WARNING);
+				plugin.getConfig().set("TownyEnabled", false);
+				return;
+			}
+			
+			Towny = (com.palmergames.bukkit.towny.Towny)townyplugin;
+			
+			return;
+		}
+	}
+
+}
