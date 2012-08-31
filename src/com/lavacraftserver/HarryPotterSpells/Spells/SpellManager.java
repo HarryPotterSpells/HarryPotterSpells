268e0b24819f919e5c6866c10cd9bf3f6645726e
Kieran Wallbanks <1millionchances@gmail.com>
2012-08-31 11:51:50 +0100

    Added basic canCast() and getCastSpellErrorMessage()

--- src/com/lavacraftserver/HarryPotterSpells/Spells/SpellManager.java
+++ src/com/lavacraftserver/HarryPotterSpells/Spells/SpellManager.java
@@ -2,7 +2,11 @@ package com.lavacraftserver.HarryPotterSpells.Spells;
 
 import java.util.ArrayList;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 import com.lavacraftserver.HarryPotterSpells.HarryPotterSpells;
+import com.palmergames.bukkit.towny.object.TownyUniverse;
 
 public class SpellManager {
 	private ArrayList<Spell> spellList;
@@ -59,6 +63,48 @@ public class SpellManager {
 			}
 		}
 		return false;
+	}
+	
+	public int canCastSpell(Player player, Spell spell, Location playerLocation, Location targetLocation) {
+		//Towny checks first
+		if(plugin.Towny.isEnabled()) {
+			if(!plugin.getConfig().getBoolean("Towny.canCastSpellInTown")) {
+				if(TownyUniverse.getTownName(playerLocation) != null) {return 1;}
+			}
+			if(!plugin.getConfig().getBoolean("Towny.canCastSpellOntoTown")) {
+				if(TownyUniverse.getTownName(targetLocation) != null) {return 2;}
+			}
+			if(!plugin.getConfig().getBoolean("Towny.canCastSpellInWilderness")) {
+				if(TownyUniverse.getTownName(playerLocation) == null) {return 3;}
+			}
+			if(!plugin.getConfig().getBoolean("Towny.canCastSpellOntoWilderness")) {
+				if(TownyUniverse.getTownName(targetLocation) == null) {return 4;}
+			}
+		}
+		//Now for WorldGuard checks
+		if(plugin.WorldGuard.isEnabled()) {
+			if(!plugin.getConfig().getBoolean("WorldGuard.canCastWhereNotAllowedToBuild")) {
+				if(!plugin.WorldGuard.WorldGuard.canBuild(player, playerLocation)) {return 5;}
+			}
+			if(!plugin.getConfig().getBoolean("WorldGuard.canCastWhereNotAllowedToBuildTarget")) {
+				if(!plugin.WorldGuard.WorldGuard.canBuild(player, targetLocation)) {return 6;}
+			}
+		}
+		
+		return 0;
+	}
+	
+	public String getCastSpellErrorMessage(int e) {
+		String errorMessage = "";
+		switch(e) {
+		case 1: errorMessage = "You cannot cast spells whilst in a Town";
+		case 2: errorMessage = "You cannot cast spells into a Town";
+		case 3: errorMessage = "You cannot cast spells whilst in the Wilderness";
+		case 4: errorMessage = "You cannot cast spells into the Wilderness";
+		case 5: errorMessage = "You cannot cast spells where you cannot build";
+		case 6: errorMessage = "You cannot cast spells into an area where you cannot build";
+		}
+		return errorMessage;
 	}
 
 }
