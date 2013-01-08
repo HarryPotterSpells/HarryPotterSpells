71425a72878bfa0c07c835ff711866d3ab157605
kezz101 <1millionchances@gmail.com>
2013-01-08 20:25:10 +0000

    Fixed formatting and import errors

--- src/com/lavacraftserver/HarryPotterSpells/Spells/Expelliarmus.java
+++ src/com/lavacraftserver/HarryPotterSpells/Spells/Expelliarmus.java
@@ -1,10 +1,13 @@
 package com.lavacraftserver.HarryPotterSpells.Spells;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
-
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 import com.lavacraftserver.HarryPotterSpells.HarryPotterSpells;
 import com.lavacraftserver.HarryPotterSpells.Spells.Spell.spell;
@@ -25,24 +28,14 @@ public class Expelliarmus extends Spell {
 	public void cast(Player p) {
 		if(Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls()) instanceof Player) {
 			Player target = (Player) Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls());
-			
-			Location targetloc = target.getLocation();
-			//We need to get the direction the target is facing, add 2, and drop it there.
-			
-			List<Integer> disarmItems = Arrays.asList({Material.STICK.getId(), Material.WOOD_SWORD.getId(),
-				Material.STONE_SWORD.getId(), Material.IRON_SWORD.getId(), Material.GOLD_SWORD.getId(),
-				Material.DIAMOND_SWORD.getId(), Material.BOW.getId()}
-			
+			Location targetloc = target.getLocation();			
+			List<Integer> disarmItems = Arrays.asList(Material.STICK.getId(), Material.WOOD_SWORD.getId(), Material.STONE_SWORD.getId(), Material.IRON_SWORD.getId(), Material.GOLD_SWORD.getId(), Material.DIAMOND_SWORD.getId(), Material.BOW.getId());
 			if (disarmItems.contains(target.getItemInHand().getTypeId())) {
 				Item i = target.getWorld().dropItem(targetloc, target.getItemInHand());
 				target.setItemInHand(null);
 				Vector vector = targetloc.getDirection();
-				i.setVelocity(new Vector
-						(vector.getX() * 2,
-						vector.getY() * 2,
-						vector.getZ() * 2));
+				i.setVelocity(new Vector(vector.getX() * 2, vector.getY() * 2, vector.getZ() * 2));
 			}
-			
 		} else {
 			plugin.PM.warn(p, "You can only cast this on players.");
 		}
