0eae92d4a0776c8f5616a7b732b8e6a6466d9def
NP98765 <np98765@infinitenight.net>
2013-05-28 20:46:18 -0700

    Fixed Glacius (now encases the target player rather than the caster)

--- src/com/lavacraftserver/HarryPotterSpells/Spells/Glacius.java
+++ src/com/lavacraftserver/HarryPotterSpells/Spells/Glacius.java
@@ -5,7 +5,9 @@ import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 
+import com.lavacraftserver.HarryPotterSpells.HPS;
 import com.lavacraftserver.HarryPotterSpells.Spells.Spell.spell;
+import com.lavacraftserver.HarryPotterSpells.Utils.Targeter;
 
 @spell (
 		name="Glacius",
@@ -25,17 +27,23 @@ public class Glacius extends Spell {
 												            BlockFace.WEST,
 												            BlockFace.NORTH_WEST
 												            };
-	public void cast(Player player) {
-		Location[] locs = new Location[]{
-										player.getLocation(),
-										player.getLocation().add(0,1,0),
-										player.getLocation().add(0,2,0)
-										};
-		for(Location loc : locs) {
-			for(BlockFace bf : surroundings){
-				loc.getBlock().getRelative(bf, 1).setType(Material.ICE);
+	@Override
+	public void cast(Player p) {
+		if(Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls()) instanceof Player) {
+			Player player = (Player) Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls());
+			Location[] locs = new Location[]{
+											player.getLocation(),
+											player.getLocation().add(0,1,0),
+											player.getLocation().add(0,2,0)
+											};
+			for(Location loc : locs) {
+				for(BlockFace bf : surroundings){
+					loc.getBlock().getRelative(bf, 1).setType(Material.ICE);
+				}
 			}
+			player.getLocation().add(0,2,0).getBlock().setType(Material.ICE);
+		} else {
+			HPS.PM.warn(p, "This may only be used on a player.");
 		}
-		player.getLocation().add(0,2,0).getBlock().setType(Material.ICE);
 	}
 }
