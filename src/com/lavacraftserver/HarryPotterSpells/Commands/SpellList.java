45a76283adc2402fce464515626a7d472a3c381e
Austin Payne <payneaustin5@gmail.com>
2013-05-29 18:28:10 -0500

    Added teachall, modified spelllist

--- src/com/lavacraftserver/HarryPotterSpells/Commands/SpellList.java
+++ src/com/lavacraftserver/HarryPotterSpells/Commands/SpellList.java
@@ -9,18 +9,22 @@ import org.bukkit.command.CommandSender;
 import com.lavacraftserver.HarryPotterSpells.HPS;
 import com.lavacraftserver.HarryPotterSpells.Spells.Spell;
 
-@HCommand (name="spelllist", description="lists all spells", usage="/spelllist", permissionDefault="true")
+@HCommand(name = "spelllist", description = "lists all spells", usage = "/spelllist", permissionDefault = "true")
 public class SpellList implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length != 0){
+		if (args.length != 0) {
 			return false;
 		}
 		ArrayList<Spell> spells = HPS.SpellManager.getSpells();
-		String spelllist = "Spells:";
-		for(Spell spell : spells){
-			spelllist = spelllist.concat(", " + spell.getName());
+		String spelllist = null;
+		for (Spell spell : spells) {
+			if (spelllist == null) {
+				spelllist = "Spells: ".concat(spell.getName());
+			} else {
+				spelllist = spelllist.concat(", " + spell.getName());
+			}
 		}
 		HPS.PM.dependantMessagingTell(sender, spelllist);
 		return true;
