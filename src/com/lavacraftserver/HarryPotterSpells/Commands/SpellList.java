c5994aee9f905d0957ff7671920499deef434361
Austin Payne <payneaustin5@gmail.com>
2013-05-29 17:59:58 -0500

    Added spelllist

---
+++ src/com/lavacraftserver/HarryPotterSpells/Commands/SpellList.java
@@ -0,0 +1,28 @@
+package com.lavacraftserver.HarryPotterSpells.Commands;
+
+import java.util.ArrayList;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.lavacraftserver.HarryPotterSpells.HPS;
+import com.lavacraftserver.HarryPotterSpells.Spells.Spell;
+
+@HCommand (name="spelllist", description="lists all spells", usage="/spelllist", permissionDefault="true")
+public class SpellList implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(args.length != 0){
+			return false;
+		}
+		ArrayList<Spell> spells = HPS.SpellManager.getSpells();
+		String spelllist = "Spells:";
+		for(Spell spell : spells){
+			spelllist = spelllist.concat(", " + spell.getName());
+		}
+		HPS.PM.dependantMessagingTell(sender, spelllist);
+		return true;
+	}
+}
