4645b6a43cf94d786e00e454a0a4466562b0b727
NP98765 <np98765@infinitenight.net>
2013-05-31 13:56:37 -0700

    Made PetrificusTotalus duration configurable.

--- src/com/lavacraftserver/HarryPotterSpells/Spells/PetrificusTotalus.java
+++ src/com/lavacraftserver/HarryPotterSpells/Spells/PetrificusTotalus.java
@@ -3,6 +3,7 @@ package com.lavacraftserver.HarryPotterSpells.Spells;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -23,12 +24,21 @@ public class PetrificusTotalus extends Spell implements Listener {
 	public static List<String> players = new ArrayList<>();
 
 	@Override
-	public void cast(Player p) {
+	public void cast(final Player p) {
 		if (Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls()) instanceof Player) {
 			PetrificusTotalus.players.add(((Player) Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls())).getName());
-			Player target = (Player) Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls());
+			final Player target = (Player) Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls());
 			Location loc = new Location(target.getWorld(), target.getLocation().getBlockX(), target.getLocation().getBlockY() + 1, target.getLocation().getBlockZ());
 			target.getWorld().createExplosion(loc, 0F);
+			
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(HPS.Plugin, new Runnable() {
+				   public void run() {
+					   if(PetrificusTotalus.players.contains(target.getName())) {
+						   PetrificusTotalus.players.remove(target.getName());
+					   } 
+				   }
+				}, HPS.Plugin.getConfig().getLong("spells.petrificustotalus.duration", 600L));
+			
 		} else {
 			HPS.PM.warn(p, "This can only be used on a player.");
 		}
