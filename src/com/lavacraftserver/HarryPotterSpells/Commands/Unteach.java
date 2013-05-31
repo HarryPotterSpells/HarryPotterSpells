e71fa9b2a73a41523c3c8356984e8a56dfd9c0ac
NP98765 <np98765@infinitenight.net>
2013-05-31 14:17:27 -0700

    Fixed up /unteach, preparing for upgrades

--- src/com/lavacraftserver/HarryPotterSpells/Commands/Unteach.java
+++ src/com/lavacraftserver/HarryPotterSpells/Commands/Unteach.java
@@ -9,31 +9,45 @@ import org.bukkit.entity.Player;
 import com.lavacraftserver.HarryPotterSpells.HPS;
 import com.lavacraftserver.HarryPotterSpells.Spells.Spell;
 
-@HCommand(name="unteach", description="Makes a player forget a spell", usage="/unteach <player> <spell>")
+@HCommand(name="unteach", description="Makes a player forget a spell", usage="/unteach <spell> [player | me]")
 public class Unteach implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if(args.length != 2)
-			return false;
+		if(args.length > 2 || args.length == 0) {
+			HPS.PM.dependantMessagingWarn(sender, "The correct usage is /unteach <spell> [player | me].");
+			return true;
+		}
 			
-		if(!HPS.SpellManager.isSpell(args[1])) {
-			HPS.PM.dependantMessagingWarn(sender, "That spell was not recognized");
+		if (!HPS.SpellManager.isSpell(args[0])) {
+			HPS.PM.dependantMessagingWarn(sender, "That spell was not recognized.");
 			return true;
 		}
 		
-		Player teachTo = Bukkit.getPlayer(args[0]);
-		Spell spell = HPS.SpellManager.getSpell(args[1]);
-		if(teachTo != null) {
-			if(spell.playerKnows(teachTo)) {
-				spell.unTeach(teachTo);
-				HPS.PM.dependantMessagingTell(sender, teachTo.getName() + " has forgotten " + spell.getName());
-			} else
-				HPS.PM.dependantMessagingWarn(sender, teachTo.getName() + " doesn't know " + spell.getName() + ".");
-		} else
+		Spell spell = HPS.SpellManager.getSpell(args[0]);
+		Player unteachTo = null;
+		
+		if ((args.length == 1) || args[1].equalsIgnoreCase("me")) {
+			if (sender instanceof Player) {
+				unteachTo = (Player) sender;
+			} else {
+				HPS.PM.dependantMessagingWarn(sender, "Please specify a player.");
+			}
+		} else {
+			unteachTo = Bukkit.getPlayer(args[1]);
+		}
+		
+		if(unteachTo != null) {
+			if(spell.playerKnows(unteachTo)) {
+				spell.unTeach(unteachTo);
+				HPS.PM.dependantMessagingTell(sender, unteachTo.getName() + " has forgotten " + spell.getName());
+			} else {
+				HPS.PM.dependantMessagingWarn(sender, unteachTo.getName() + " doesn't know " + spell.getName() + ".");
+			}
+		} else {
 			HPS.PM.dependantMessagingTell(sender, "The player was not found.");
+		}
 		
 		return true;
 	}
-
 }
