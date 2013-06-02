07ddff420fc94486fce25f81db79b5b4c945f25e
kezz101 <1millionchances@gmail.com>
2013-06-02 18:01:54 +0100

    Added particle utility class

--- src/com/lavacraftserver/HarryPotterSpells/Utils/SVPBypass.java
+++ src/com/lavacraftserver/HarryPotterSpells/Utils/SVPBypass.java
@@ -1,5 +1,7 @@
 package com.lavacraftserver.HarryPotterSpells.Utils;
 
+import java.lang.reflect.Field;
+
 import com.lavacraftserver.HarryPotterSpells.HPS;
 
 /**
@@ -43,6 +45,32 @@ public class SVPBypass {
         } catch (ClassNotFoundException e) {
             return null;
         }
+    }
+    
+    /**
+    * Sets a value of an {@link Object} via reflection
+    * @param instance instance the class to use
+    * @param fieldName the name of the {@link Field} to modify
+    * @param value the value to set
+    * @throws Exception if an error occurred during reflections
+    */
+    public static void setValue(Object instance, String fieldName, Object value) throws Exception {
+        Field field = instance.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(instance, value);
+    }
+ 
+    /**
+    * Get a value of an {@link Object}'s {@link Field}
+    * @param instance the target {@link Object}
+    * @param fieldName name of the {@link Field}
+    * @return the value of {@link Object} instance's {@link Field} with the name of fieldName
+    * @throws Exception if an error occurred during reflections
+    */
+    public static Object getValue(Object instance, String fieldName) throws Exception {
+        Field field = instance.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        return field.get(instance);
     }
 
 }
