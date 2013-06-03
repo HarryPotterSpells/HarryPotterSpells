7052aba87a54fee14459a45de2d5b18a396fb3b8
kezz101 <1millionchances@gmail.com>
2013-06-03 21:50:08 +0100

    Added a PSC auto updater

--- src/com/lavacraftserver/HarryPotterSpells/PlayerSpellConfig.java
+++ src/com/lavacraftserver/HarryPotterSpells/PlayerSpellConfig.java
@@ -4,19 +4,32 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.logging.Level;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
+
+import com.lavacraftserver.HarryPotterSpells.Jobs.EnableJob;
 
 /**
  * A class that manages the {@code PlayerSpellConfig.yml} file. <br>
  * This file is used to store all the spells that players know.
  */
-public class PlayerSpellConfig {
+public class PlayerSpellConfig implements EnableJob {
 	private FileConfiguration PSC = null;
 	private File PSCFile = null;
+	
+	/**
+	 * The current version specifying the format of the {@code PlayerSpellConfig.yml}
+	 */
+	public static final double CURRENT_VERSION = 0.4d;
 	
 	/**
 	 * Reloads the PlayerSpellConfig
@@ -67,5 +80,64 @@ public class PlayerSpellConfig {
 	public List<String> getStringListOrEmpty(String string) {
 	    return getPSC().getStringList(string) == null ? new ArrayList<String>() : getPSC().getStringList(string);
 	}
+	
+	@Override
+	public void onEnable(PluginManager pm) {
+	    Double version = getPSC().getDouble("VERSION_DO_NOT_EDIT", -1d) == -1d ? null : getPSC().getDouble("VERSION_DO_NOT_EDIT", -1d);
+	    if(version == null || version < CURRENT_VERSION) {
+	        HPS.PM.log(Level.INFO, "The PlayerSpellConfig.yml is out of date! Updating file...");
+	        
+	        // STORE UPDATES HERE
+	        
+	        if(version == null) { // Updating from unformatted version to version 0.4
+	            Map<String, List<String>> newConfig = new HashMap<String, List<String>>(), lists = new HashMap<String, List<String>>();
+	            Set<String> css = new HashSet<String>();
+	            
+	            for(String s : getPSC().getKeys(false)) // This seemingly stupid code is to avoid a ConcurrencyModificationException (google it)
+	                css.add(s);
+	            
+	            for(String s : css)
+	                lists.put(s, getPSC().getStringList(s));
+	            
+	            for(String cs : css) {
+	                List<String> list = new ArrayList<String>();
+	                for(String spellString : getPSC().getStringList(cs)) {
+	                    if(spellString.equals("AlarteAscendare")) {
+                            list.add("Alarte Ascendare");
+	                    } else if(spellString.equals("AvadaKedavra")) {
+	                        list.add("Avada Kedavra");
+	                    } else if(spellString.equals("FiniteIncantatem")) {
+                            list.add("Finite Incantatem");
+	                    } else if(spellString.equals("MagnaTonitrus")) {
+                            list.add("Magna Tonitrus");
+                        } else if(spellString.equals("PetrificusTotalus")) {
+                            list.add("Petrificus Totalus");
+                        } else if(spellString.equals("TimeSpell")) {
+	                        list.add("Time");
+	                    } else if(spellString.equals("TreeSpell")) {
+                            list.add("Tree");
+	                    } else if(spellString.equals("WingardiumLeviosa")) {
+                            list.add("Wingardium Leviosa");
+	                    } else {
+	                        list.add(spellString);
+	                    }
+	                }
+	                
+	                newConfig.put(cs, list);
+	            }
 
+	            for(Entry<String, List<String>> ent : newConfig.entrySet()) {
+	                getPSC().set(ent.getKey(), ent.getValue());
+	            }
+	            
+	            getPSC().set("VERSION_DO_NOT_EDIT", 0.4d);
+	            
+	            savePSC();
+	            
+	            HPS.PM.log(Level.INFO, "The PlayerSpellConfig.yml has been updated to version 0.4");
+	        }
+	        
+	    }
+	}
+	
 }
