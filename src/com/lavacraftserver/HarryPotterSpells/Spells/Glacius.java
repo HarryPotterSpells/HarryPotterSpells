09732408ca4ff1149b1b17f4f17c16fb45e4667a
NP98765 <np98765@infinitenight.net>
2013-06-07 18:25:42 -0700

    Glacius now saves and restores blocks replaced by ice.

--- src/com/lavacraftserver/HarryPotterSpells/Spells/Glacius.java
+++ src/com/lavacraftserver/HarryPotterSpells/Spells/Glacius.java
@@ -1,10 +1,16 @@
 package com.lavacraftserver.HarryPotterSpells.Spells;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
+import org.bukkit.material.Chest;
 
 import com.lavacraftserver.HarryPotterSpells.HPS;
 import com.lavacraftserver.HarryPotterSpells.Spells.Spell.spell;
@@ -19,6 +25,7 @@ import com.lavacraftserver.HarryPotterSpells.Utils.Targeter;
 )
 public class Glacius extends Spell {
 	
+	HashMap<Location, Material> blocks = new HashMap<Location, Material>();
 	private final BlockFace[] surroundings = new BlockFace[]{
 															BlockFace.NORTH, 
 												            BlockFace.NORTH_EAST,
@@ -44,18 +51,51 @@ public class Glacius extends Spell {
 			Location loc = new Location(world, playerX, playerY, playerZ, pitch, yaw);
 			player.teleport(loc);
 			
-			Location[] locations = new Location[]{
+			final Location[] locations = new Location[]{
 											player.getLocation(),
 											player.getLocation().add(0,1,0),
 											player.getLocation().add(0,2,0)
 											};
-			for(Location locs : locations) {
-				for(BlockFace bf : surroundings){
-					locs.getBlock().getRelative(bf, 1).setType(Material.ICE);
+			
+			final Location top = player.getLocation().add(0,2,0);
+			blocks.put(top, top.getBlock().getType());
+			top.getBlock().setType(Material.ICE);
+			
+			for (Location locs : locations) {
+				for (BlockFace bf : surroundings) {
+					
+					final Block relative = locs.getBlock().getRelative(bf, 1);
+					
+					blocks.put(relative.getLocation(), relative.getType());
+					relative.setType(Material.ICE);
+	
 				}
+				
 			}
-			player.getLocation().add(0,2,0).getBlock().setType(Material.ICE);
+			
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(HPS.Plugin, new Runnable() {
+				public void run() {
+					
+					for (Location locs : locations) {
+						for (BlockFace bf : surroundings) {
+							final Block relative = locs.getBlock().getRelative(bf, 1);
+							
+							relative.setType(blocks.get(relative.getLocation()));
+							blocks.remove(relative.getLocation());
+							
+						}
+						
+					}
+					
+					top.getBlock().setType(blocks.get(top));
+					blocks.remove(top);
+
+				}
+				
+			}, HPS.Plugin.getConfig().getLong("spells.glacius.ice-duration", 40L));
+			
 			return true;
+			
 		} else {
 			HPS.PM.warn(p, "This can only be used on a player.");
 			return false;
