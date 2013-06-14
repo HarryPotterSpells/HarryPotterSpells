d2dd8eca7b4e31c6ba9fadfb2d305df7c1055ec4
kezz101 <1millionchances@gmail.com>
2013-06-14 13:09:08 +0100

    Made Avis configurable and made it use bats too (also configurable)!

--- src/com/lavacraftserver/HarryPotterSpells/Spells/Avis.java
+++ src/com/lavacraftserver/HarryPotterSpells/Spells/Avis.java
@@ -21,10 +21,14 @@ public class Avis extends Spell {
     }
 
     public boolean cast(Player p) {
-		int chickenAmount = (Integer) getConfig("chickens", 5);
-		for(int i=0; i <= chickenAmount; i++){
+		int chickenAmount = (Integer) getConfig("chickens.amount", 5), batAmount = (Integer) getConfig("bats.amount", 0), chickenVelocity = (Integer) getConfig("chickens.velocity", 2), batVelocity = (Integer) getConfig("bats.velocity", 2);
+		for(int i = 0; i <= chickenAmount; i++) {
 			Entity mob = p.getWorld().spawnEntity(p.getEyeLocation(), EntityType.CHICKEN);
-			mob.setVelocity(p.getEyeLocation().getDirection().multiply(2));
+			mob.setVelocity(p.getEyeLocation().getDirection().multiply(chickenVelocity));
+		}
+		for(int i = 0; i <= batAmount; i++) {
+		    Entity mob = p.getWorld().spawnEntity(p.getEyeLocation(), EntityType.BAT);
+		    mob.setVelocity(p.getEyeLocation().getDirection().multiply(batVelocity));
 		}
 		return true;
 	}
