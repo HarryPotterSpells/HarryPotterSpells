5e61213ec58d8665accbf9ad3a8ab250e15a75a0
Zach Sugano <zachoooo@gmail.com>
2013-07-06 23:54:15 -0700

    Allowing Expelliarmus to get living entities as well

--- src/com/hpspells/core/spell/Expelliarmus.java
+++ src/com/hpspells/core/spell/Expelliarmus.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.util.Vector;
 
@@ -27,16 +28,16 @@ public class Expelliarmus extends Spell {
     }
 
     public boolean cast(Player p) {
-		if(Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls()) instanceof Player) {
-			Player target = (Player) Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls());
+		if(Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls()) != null) {
+			LivingEntity target = Targeter.getTarget(p, this.getRange(), this.canBeCastThroughWalls());
 			Location targetloc = target.getLocation();			
 			List<Integer> disarmItems = Arrays.asList(HPS.Wand.getWand().getTypeId());
 			if((Boolean) getConfig("disarm-weapons", true))
 				disarmItems.addAll(Arrays.asList(Material.STICK.getId(), Material.WOOD_SWORD.getId(), Material.STONE_SWORD.getId(), Material.IRON_SWORD.getId(), Material.GOLD_SWORD.getId(), Material.DIAMOND_SWORD.getId(), Material.BOW.getId()));
 
-			if (disarmItems.contains(target.getItemInHand().getTypeId())) {
-				Item i = target.getWorld().dropItem(targetloc, target.getItemInHand());
-				target.setItemInHand(null);
+			if (disarmItems.contains(target.getEquipment().getItemInHand().getTypeId())) {
+				Item i = target.getWorld().dropItem(targetloc, target.getEquipment().getItemInHand());
+				target.getEquipment().setItemInHand(null);
 				Vector vector = targetloc.getDirection();
 				i.setVelocity(new Vector(vector.getX() * 2, vector.getY() * 2, vector.getZ() * 2));
 			}
