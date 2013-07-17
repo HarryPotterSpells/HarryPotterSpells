6431dd2e5ec94ddb11d27597134e67b04db9562d
jack lin <jacklin213@gmail.com>
2013-07-18 10:29:23 +1200

    Updated MgnaTonitrus, Added Arania Exumai Spell

---
+++ src/com/hpspells/core/spell/AraniaExumai.java
@@ -0,0 +1,51 @@
+package com.hpspells.core.spell;
+
+import org.bukkit.Effect;
+import org.bukkit.block.Block;
+import org.bukkit.entity.CaveSpider;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Spider;
+
+import com.hpspells.core.HPS;
+import com.hpspells.core.SpellTargeter.SpellHitEvent;
+import com.hpspells.core.spell.Spell.SpellInfo;
+
+@SpellInfo(
+		name="Arania Exumai",
+		description="descAraniaExumai",
+		range=50,
+		goThroughWalls=false,
+		cooldown=90
+)
+public class AraniaExumai extends Spell{
+
+	public AraniaExumai(HPS instance) {
+		super(instance);
+	}
+
+	@Override
+	public boolean cast(final Player p) {
+		HPS.SpellTargeter.register(p, new SpellHitEvent(){
+
+			@Override
+			public void hitBlock(Block block) {
+				HPS.PM.warn(p, HPS.Localisation.getTranslation("spellLivingEntityOnly"));
+			}
+
+			@Override
+			public void hitEntity(LivingEntity entity) {
+				//In case if one check doesn't work, check the other way as well
+				if ((entity instanceof Spider) || (entity.getType() == EntityType.SPIDER) || (entity instanceof CaveSpider) || (entity.getType() == EntityType.CAVE_SPIDER)){
+					// Do not cast Spider to entity because it can be a cave spider
+					entity.damage(6);
+					int knockback = (Integer) getConfig("knockback", 10);
+					entity.setVelocity(entity.getVelocity().add(entity.getLocation().toVector().subtract(p.getLocation().toVector().normalize().multiply(knockback))));
+				}
+			}
+			
+		}, 1.5, Effect.BLAZE_SHOOT);
+		return true;
+	}
+}
