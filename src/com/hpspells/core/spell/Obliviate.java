13b643cedc3d69805c7c0432dfba4e60e11981f6
Zach Sugano <zachoooo@gmail.com>
2013-07-31 11:00:10 -0700

    Null pointer check

--- src/com/hpspells/core/spell/Obliviate.java
+++ src/com/hpspells/core/spell/Obliviate.java
@@ -8,7 +8,7 @@ import com.hpspells.core.SpellTargeter.SpellHitEvent;
 import com.hpspells.core.util.ParticleEffect;
 
 public class Obliviate extends Spell {
-	
+
 	public Obliviate(com.hpspells.core.HPS instance) {
 		super(instance);
 	}
@@ -20,23 +20,28 @@ public class Obliviate extends Spell {
 			@Override
 			public void hitBlock(Block block) {
 				HPS.PM.warn(p, HPS.Localisation.getTranslation("spellPlayerOnly"));
-				
+
 			}
 
 			@Override
 			public void hitEntity(LivingEntity entity) {
 				if (entity instanceof Player) {
 					Player target = (Player) entity;
-					HPS.SpellManager.setCoolDown(target.getName(), HPS.SpellManager.getCurrentSpell(target));
-					String message = "The cooldown for the spell " + HPS.SpellManager.getCurrentSpell(target).getName() + " has been reset for " + target.getName(); 
-					HPS.PM.warn(p, message);
-					HPS.PM.warn(target, message);
+					Spell spell = HPS.SpellManager.getCurrentSpell(target);
+					if (spell == null) {
+						HPS.PM.warn(p, "That player doesn't know any spells or has none selected!");
+					} else {
+						HPS.SpellManager.setCoolDown(target.getName(), spell);
+						String message = "The cooldown for the spell " + spell.getName() + " has been reset for " + target.getName(); 
+						HPS.PM.warn(p, message);
+						HPS.PM.warn(target, message);
+					}
 				} else {
 					HPS.PM.warn(p, HPS.Localisation.getTranslation("spellPlayerOnly"));
 				}
-				
+
 			}
-			
+
 		}, 1.0, ParticleEffect.LARGE_SMOKE);
 		return false;
 	}
