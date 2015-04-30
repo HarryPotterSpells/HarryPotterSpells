ddec13ad0fbc9b7d28ba51f6dbd2cc32d5e1e8b3
Jack Lin <jacklin213@gmail.com>
2015-04-30 13:49:05 +1200

    Add APIHandler for extensions to access classes

--- src/com/hpspells/core/HPS.java
+++ src/com/hpspells/core/HPS.java
@@ -31,6 +31,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.hpspells.core.Metrics.Graph;
 import com.hpspells.core.Metrics.Plotter;
+import com.hpspells.core.api.APIHandler;
 import com.hpspells.core.api.event.SpellBookRecipeAddEvent;
 import com.hpspells.core.command.CommandInfo;
 import com.hpspells.core.command.HCommandExecutor;
@@ -76,6 +77,7 @@ public class HPS extends JavaPlugin {
             SpellManager = new SpellManager(this);
             Wand = new Wand(this);
             ExtensionManager = new ExtensionManager(this);
+            new APIHandler(this);
             
             // Configuration
             ConfigurationManager.loadConfig();
